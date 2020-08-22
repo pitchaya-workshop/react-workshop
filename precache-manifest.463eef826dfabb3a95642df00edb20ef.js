95fbea2f51dccf91173a62f2f3ff36139c6523d4
nop <nopzaaaaa@hotmail.com>
2020-08-22 16:40:15 +0700

    Updates

---
+++ precache-manifest.463eef826dfabb3a95642df00edb20ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71c89a03b8ca6664497640761bd6ce6",
+    "url": "/react-workshop/index.html"
+  },
+  {
+    "revision": "ba031fd31f57cba683b6",
+    "url": "/react-workshop/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7ce766edeaa237cf0ac9",
+    "url": "/react-workshop/static/css/main.b28ff1cb.chunk.css"
+  },
+  {
+    "revision": "ba031fd31f57cba683b6",
+    "url": "/react-workshop/static/js/2.dc79986a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-workshop/static/js/2.dc79986a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ce766edeaa237cf0ac9",
+    "url": "/react-workshop/static/js/main.ac4970a6.chunk.js"
+  },
+  {
+    "revision": "28969ed021637d9fd097",
+    "url": "/react-workshop/static/js/runtime-main.223fd330.js"
+  }
+]);
